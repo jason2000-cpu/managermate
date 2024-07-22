9baa7ea4cbbc107656e7af888075c2f16041fa79
jason2000-cpu <jasonmwangi2000@gmail.com>
2024-07-22 20:49:32 +0300

    added dark theme

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -4,11 +4,23 @@ import React, { useState } from  'react'
 import { BsPersonWorkspace } from "react-icons/bs";
 import { IoNotifications } from "react-icons/io5";
 import { IoSearch } from "react-icons/io5";
+import { MdLightMode, MdModeNight, MdNightlife } from 'react-icons/md';
 
 
 function TopBar(){
+    const [ theme, setTheme ] = useState('dark');
+
+    // const toggleTheme = () => {
+    //     theme === 'dark' ? setTheme('light') : setTheme('dark')
+    //     alert("Current Theme", theme)
+    // }
+    const toggleTheme = () =>{
+        alert("Current Theme", theme)
+        document.documentElement.classList.toggle('dark');
+    }
+    
     return (
-        <div className='border px-10 flex justify-between w-full py-2 shadow-md'>
+        <div className='border px-10 flex justify-between w-full py-2 shadow-md dark:bg-gray-500 dark:text-white'>
             <div className='flex space-x-2'>
                 <BsPersonWorkspace size={24} color='#083778' />
                 <span className='font-bold text-[#083778]'>
@@ -21,6 +33,10 @@ function TopBar(){
                     <IoSearch size={24}  color='#083778' className='absolute top-3 right-24'/>
                 </div>
                 <IoNotifications size={24}  className='cursor-pointer'/>
+                {
+                    theme == 'light' ? <MdModeNight  size={24} onClick={toggleTheme} className='cursor-pointer'/> : 
+                                    <MdLightMode size={24} onClick={toggleTheme} className='cursor-pointer' />
+                }
             </div>
         </div>
     )
